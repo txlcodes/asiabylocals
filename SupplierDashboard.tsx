5cd6ffe59242ae7828d6fc7b76222bfeb1cfc881
Talha <talha@yourmail.com>
2026-01-29 16:45:02 +0530

    Remove bottom padding from header navbar
    - Changed from py-6 to pt-6 pb-0
    - Removes padding from below the header as requested

--- SupplierDashboard.tsx
+++ SupplierDashboard.tsx
@@ -354,7 +354,7 @@ const SupplierDashboard: React.FC<SupplierDashboardProps> = ({ supplier, onLogou
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
       <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
-        <div className="max-w-7xl mx-auto px-6 py-6">
+        <div className="max-w-7xl mx-auto px-6 pt-6 pb-0">
           <div className="flex items-center justify-between">
             {/* Left side - Logo */}
             <div className="flex items-center flex-1">
